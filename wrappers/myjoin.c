4c77f49c4d3698d9529c2bd479a17be2fa555f55
Austin Herring <awh44@drexel.edu>
2015-08-22 20:33:26 -0400

    Added myjoin wrapper.

---
+++ wrappers/myjoin.c
@@ -0,0 +1,24 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Please incldue the pid as a command line parameter.\n");
+		return 1;
+	}
+
+	pid_t pid = atol(argv[1]);
+	long success = syscall(290, pid);
+	if (success < 0)
+	{
+		perror("Failure in myjoin");
+		return 2;
+	}
+
+	printf("Success! Process joined target and has finished waiting.\n");
+	return 0;
+}
