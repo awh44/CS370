ba2c24add6ebb0b131e297856fa2336d999ff5b2
Austin Herring <awh44@drexel.edu>
2015-09-13 13:42:02 -0400

    Removed testing printfs from 12-bit reads

--- FAT12/src/fat12.c
+++ FAT12/src/fat12.c
@@ -40,10 +40,6 @@ uint8_t read_twelve_bits_twice(int fd, uint16_t *integers)
 		return 0;
 	}
 
-	printf("byte 0: 0x%x\n", tmp[0]);
-	printf("byte 1: 0x%x\n", tmp[1]);
-	printf("byte 2; 0x%x\n", tmp[2]);
-
 	integers[0] = (((uint16_t) tmp[1] & 0x0f) << 8) | tmp[0];
 	integers[1] = ((uint16_t ) tmp[2] << 4) | ((tmp[1] & 0xf0) >> 4);
 	
