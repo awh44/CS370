6a0ffca8f69a0e2932cad9a70e0d660fc2631d7e
Austin Herring <awh44@drexel.edu>
2015-09-23 13:27:40 -0400

    Fixed up main msdosdir.c program; updated makefile to reflect.

--- FAT12/src/msdosdir.c
+++ FAT12/src/msdosdir.c
@@ -4,20 +4,29 @@
 
 #include "../include/fat12.h"
 
+#define USER_ERROR 1
+#define SYSTEM_ERROR 2
+
 int main(int argc, char *argv[])
 {
-	int fd = open("input/samplefat.bin", O_RDONLY, NULL);
+	if (argc < 2)
+	{
+		printf("Please include the input file as a command line argument.\n");
+		return USER_ERROR;
+	}
+
+	int fd = open(argv[1], O_RDONLY, NULL);
 	if (fd < 0)
 	{
 		perror("Could not open input file");
-		return 1;
+		return SYSTEM_ERROR;
 	}
 
 	fat12_t fat;
 	if (!read_fat12(fd, &fat))
 	{
 		perror("Could not read file system");
-		return 1;
+		return SYSTEM_ERROR;
 	}
 
 	print_fat12(&fat);
